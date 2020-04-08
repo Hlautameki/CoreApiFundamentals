ddc8b5c44a172c363d73c971e971fc752f51983e
Hlautameki <hlautameki@Mateuszs-MacBook-Pro.home>
2020-04-08 16:53:15 +0200

    Api versioning

--- src/Startup.cs
+++ src/Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Hosting;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
+using CoreCodeCamp.Controllers;
 
 namespace CoreCodeCamp
 {
@@ -23,6 +26,23 @@ namespace CoreCodeCamp
             services.AddDbContext<CampContext>();
             services.AddScoped<ICampRepository, CampRepository>();
             services.AddAutoMapper(typeof(Startup));
+
+            services.AddApiVersioning(opt =>
+            {
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 1);
+                opt.ReportApiVersions = true;
+                //opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+                //opt.ApiVersionReader = ApiVersionReader.Combine(
+                //    new HeaderApiVersionReader("X-Version"),
+                //    new QueryStringApiVersionReader("ver"));
+
+                opt.Conventions.Controller<TalksController>()
+                    .HasApiVersion(1, 0)
+                    .HasApiVersion(1, 1)
+                    .Action(c => c.Delete(default(string), default(int)))
+                        .MapToApiVersion(1, 1);
+            });
 
             services.AddMvc();
 
